d0c7077f37931bc7696e9efebfc60f2746ed8635
Rigot Florent <florent.journal@gmail.com>
2019-04-05 13:58:53 +0300

    Add first Test

---
+++ src/test/java/fi/vamk/e1800817/reactspring/model/GroupTest.java
@@ -0,0 +1,9 @@
+package fi.vamk.e1800817.reactspring.model;
+import org.junit.Test;
+
+public class GroupTest {
+    @Test 
+    public void justTesting() {
+        System.out.println("------Test, test, test");
+    }
+}
